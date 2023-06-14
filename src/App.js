d8abc1c340cf9cdfaea21ce695c2418b4128571c
Mizael55 <misaellsoler@gmail.com>
2023-06-14 17:20:36 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -2,63 +2,46 @@
 import './App.css';
 import Button from './components/button';
 import Screen from './components/screen';
-import ButtonClear from './components/button_clear';
 import { useState } from 'react';
 import { evaluate } from 'mathjs';
 
 
 function App() {
   const [input, setInput] = useState('');
+  
 
-  const addToInput = (val) => {
-    setInput(input +  val)
+  function addToInputAndEvaluateAndClear(val) {
+    if (val === '=') {
+      setInput(evaluate(input));
+    } else if (val === 'Clear') {
+      setInput('');
+
+    } else {
+      setInput(input + val);
+    }
   }
 
-  const handlerClear = () => {
-    setInput('');
-  }
 
-  const handleEqual = () => {
-    if (input === '') return;
-    setInput(evaluate(input));
-  } 
+  let values = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*', '=', 0, '.', '/', 'Clear'];
+
 
   return (
-    <div className="App">
-
-      <div className='container-calculator'>
+    <div className="">
+      <div className=" ">
         <Screen input={input} />
-        <div className='line'>
-          <Button handleClick={addToInput}>1</Button>
-          <Button handleClick={addToInput}>2</Button>
-          <Button handleClick={addToInput}>3</Button>
-          <Button handleClick={addToInput}>+</Button>
-        </div>
-        <div className='line'>
-          <Button handleClick={addToInput}>4</Button>
-          <Button handleClick={addToInput}>5</Button>
-          <Button handleClick={addToInput}>6</Button>
-          <Button handleClick={addToInput}>-</Button>
-        </div>
-        <div className='line'>
-          <Button handleClick={addToInput}>7</Button>
-          <Button handleClick={addToInput}>8</Button>
-          <Button handleClick={addToInput}>9</Button>
-          <Button handleClick={addToInput}>*</Button>
-        </div>
-        <div className='line'>
-          <Button handleClick={handleEqual}>=</Button>
-          <Button handleClick={addToInput}>0</Button>
-          <Button handleClick={addToInput}>.</Button>
-          <Button handleClick={addToInput}>/</Button>
-        </div>
-        <div className='line'>
-          <ButtonClear handlerClear={handlerClear}>clear</ButtonClear>
+        <div >
+          <div>
+            {values.map((value, index) => {
+              return <Button key={index} handleClick={addToInputAndEvaluateAndClear}  >{ value }</Button>
+            })}
+          </ div>
         </div>
       </div>
     </div>
   );
+
+
 }
 
 export default App
-;
+  ;
--- src/components/button.js
+++ src/components/button.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import '../styles/button.css';
 
-function Button(props) {
+function Button({handleClick, ...props}) {
 
     const isOperator = val => {
-        return isNaN(val) && (val !== ".") && (val !== "=");
+        return isNaN(val) && (val !== ".") && (val !== "=") ;
     }
     return (
         <div
-            onClick={() => props.handleClick(props.children)}
+            onClick={() => handleClick(props.children)}
             className={`button-container ${isOperator(props.children) ? 'operator' : ''}`.trimEnd()}>
             {props.children}
         </div>
